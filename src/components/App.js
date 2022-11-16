9dc833de73266a119eda74575b68b2f04fff49d2
Eric Song <ericksong91@gmail.com>
2022-11-16 15:24:42 -0500

    Added components folder, moved files around

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
