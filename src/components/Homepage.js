67cf18dfc61a641d4134429003186e5c1a19089a
Eric Song <ericksong91@gmail.com>
2022-11-18 17:46:29 -0500

    Added link to penguin page from homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,15 +1,19 @@
 import React from "react";
 import penguinPic from "../images/Penguins_collage.png"
+import PenguinList from "./PenguinsList";
 import {
-    Link
+  Link
 } from "react-router-dom";
 
 function Homepage() {
   return (
     <div className="home">
-        <h1>Welcome to Penguins Galore!</h1>
-        <img src={penguinPic} />
-        <p>Here at Penguins Galore, we are dedicated to delivering some awesome penguins for you to enjoy!</p>
+      <h1>Welcome to Penguins Galore!</h1>
+      <img src={penguinPic} alt="penguin collage" />
+      <p>Here at Penguins Galore, we are dedicated to delivering some awesome penguins for you to enjoy!</p>
+      <Link to="/penguins" style={{ padding: 5 }}>
+        <button>Click Here to See Penguins!</button>
+      </Link>
     </div>
   );
 }
