eaf5c5c8be15e6f8cac3ee9d021dda39dd84a63b
Eric Song <ericksong91@gmail.com>
2022-11-20 18:04:00 -0500

    Added more to about.js

--- src/components/About.js
+++ src/components/About.js
@@ -9,8 +9,8 @@ function About() {
             <img src={image} alt="Image of Eric Song" />
             <p>Eric Song is a student at Flat Iron School currently working through the software engineering course. 
                 He is a former graduate from Penn State University with a B.S. in Chemical Engineering and previously worked in
-                the nuclear materials and aerospace defense industries. This project was created using React to demonstrate a basic
-                single page application.
+                the nuclear materials and aerospace defense industries. This project was created using the React JS library 
+                to demonstrate a basic single page application.
             </p>
             <br/>
         </div>
