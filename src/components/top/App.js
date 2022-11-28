293089654c63ed385578d619719a18123f2b189c
Eric Song <ericksong91@gmail.com>
2022-11-28 15:47:34 -0500

    Minor changes

--- src/components/top/App.js
+++ src/components/top/App.js
@@ -41,9 +41,9 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/penguins/:id" element={<PenguinPage penguins={penguins} onLiked={handleLikes} />} />
-        <Route path="/penguins/*" element={<PenguinList penguins={penguins} onSubmit={handleNewSubmit} onLiked={handleLikes} />} />
+        <Route path="/penguins/" element={<PenguinList penguins={penguins} onSubmit={handleNewSubmit} onLiked={handleLikes} />} />
         <Route path="/about" element={<About />} />
+        <Route path="/penguins/:id" element={<PenguinPage penguins={penguins} onLiked={handleLikes} />} />
       </Routes>
     </div>
   );
